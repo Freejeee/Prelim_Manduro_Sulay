8ecb3fd5cdadefacbfe1778b62318b649cdd84f5
Freejeee <gigisulay@gmail.com>
2019-12-15 17:41:20 +0800

    Add project files.

---
+++ Prelim_Manduro_Sulay/Quiz.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Quiz
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.a = New System.Windows.Forms.Button()
+        Me.b = New System.Windows.Forms.Button()
+        Me.c = New System.Windows.Forms.Button()
+        Me.d = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Pause = New System.Windows.Forms.Button()
+        Me.Go = New System.Windows.Forms.Button()
+        Me.LabelT = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 450)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button4.Location = New System.Drawing.Point(0, 178)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(200, 53)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Exit"
+        Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button3.Location = New System.Drawing.Point(0, 125)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(200, 53)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Reset"
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button1.Location = New System.Drawing.Point(0, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 53)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Change Name"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(200, 72)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(327, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Question"
+        '
+        'a
+        '
+        Me.a.Location = New System.Drawing.Point(331, 155)
+        Me.a.Name = "a"
+        Me.a.Size = New System.Drawing.Size(185, 60)
+        Me.a.TabIndex = 2
+        Me.a.Text = "Button5"
+        Me.a.UseVisualStyleBackColor = True
+        '
+        'b
+        '
+        Me.b.Location = New System.Drawing.Point(604, 155)
+        Me.b.Name = "b"
+        Me.b.Size = New System.Drawing.Size(185, 60)
+        Me.b.TabIndex = 3
+        Me.b.Text = "Button2"
+        Me.b.UseVisualStyleBackColor = True
+        '
+        'c
+        '
+        Me.c.Location = New System.Drawing.Point(331, 264)
+        Me.c.Name = "c"
+        Me.c.Size = New System.Drawing.Size(185, 60)
+        Me.c.TabIndex = 4
+        Me.c.Text = "Button6"
+        Me.c.UseVisualStyleBackColor = True
+        '
+        'd
+        '
+        Me.d.Location = New System.Drawing.Point(604, 264)
+        Me.d.Name = "d"
+        Me.d.Size = New System.Drawing.Size(185, 60)
+        Me.d.TabIndex = 5
+        Me.d.Text = "Button7"
+        Me.d.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Pause
+        '
+        Me.Pause.Location = New System.Drawing.Point(750, 379)
+        Me.Pause.Name = "Pause"
+        Me.Pause.Size = New System.Drawing.Size(75, 32)
+        Me.Pause.TabIndex = 6
+        Me.Pause.Text = "Pause"
+        Me.Pause.UseVisualStyleBackColor = True
+        '
+        'Go
+        '
+        Me.Go.Location = New System.Drawing.Point(750, 379)
+        Me.Go.Name = "Go"
+        Me.Go.Size = New System.Drawing.Size(75, 32)
+        Me.Go.TabIndex = 7
+        Me.Go.Text = "Go"
+        Me.Go.UseVisualStyleBackColor = True
+        '
+        'LabelT
+        '
+        Me.LabelT.AutoSize = True
+        Me.LabelT.ForeColor = System.Drawing.Color.White
+        Me.LabelT.Location = New System.Drawing.Point(849, 52)
+        Me.LabelT.Name = "LabelT"
+        Me.LabelT.Size = New System.Drawing.Size(56, 20)
+        Me.LabelT.TabIndex = 8
+        Me.LabelT.Text = "Timer :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(912, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 20)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Label2"
+        '
+        'Quiz
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(44, Byte), Integer), CType(CType(44, Byte), Integer), CType(CType(44, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1095, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LabelT)
+        Me.Controls.Add(Me.Go)
+        Me.Controls.Add(Me.Pause)
+        Me.Controls.Add(Me.d)
+        Me.Controls.Add(Me.c)
+        Me.Controls.Add(Me.b)
+        Me.Controls.Add(Me.a)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Quiz"
+        Me.Text = "e"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents a As Button
+    Friend WithEvents b As Button
+    Friend WithEvents c As Button
+    Friend WithEvents d As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Pause As Button
+    Friend WithEvents Go As Button
+    Friend WithEvents LabelT As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Prelim_Manduro_Sulay/Quiz.vb
@@ -0,0 +1,243 @@
+﻿Public Class Quiz
+    'For Timer
+    Dim min As Integer
+    Dim sec As Integer
+    'contain questions
+    Dim ques(15) As String
+
+    'contain the possible choices of the user
+    '0-14 will contain choices for a
+    '15-29 will contain choices for b
+    '30-44 will contain choices for c
+    '45-59nwill contain choices for d
+    Dim choices(60) As String
+
+
+    'contains answers
+    Dim answers(15) As Char
+
+    'contains user score
+    Dim score As Integer
+
+    'contains index
+    Dim startNum As Integer
+
+    'flag to indicate when to proceed and what to do
+    '1 is to progress  the ctr of questions
+    '2 is to create a new question
+    '0 default
+    Dim flag As Integer
+
+    'main
+    Private Sub Quiz_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Main()
+        choices(0) = "Percentage"
+        choices(1) = "Fruit"
+        choices(2) = "Comforter"
+        choices(3) = "3 days"
+        choices(4) = "Alex"
+        choices(5) = "Hell"
+        choices(6) = "America"
+        choices(7) = "Mercury"
+        choices(8) = "China"
+        choices(9) = "Philippines"
+        choices(10) = "Yahoo"
+        choices(11) = "Elmo"
+        choices(12) = "Cheese"
+        choices(13) = "Mars and Murrie"
+        choices(14) = "Mounain Dew"
+        choices(15) = "Hashtag"
+        choices(16) = "Color"
+        choices(17) = "Bed sheet"
+        choices(18) = "3 years"
+        choices(19) = "Sid"
+        choices(20) = "Oslo"
+        choices(21) = "Brazil"
+        choices(22) = "Saturn"
+        choices(23) = "Russia"
+        choices(24) = "India"
+        choices(25) = "Google"
+        choices(26) = "Spongebob"
+        choices(27) = "Beer"
+        choices(28) = "Mix and Match"
+        choices(29) = "Sprite"
+        choices(30) = "Question mark"
+        choices(31) = "Person"
+        choices(32) = "Socks"
+        choices(33) = "3 centuries"
+        choices(34) = "Freddie"
+        choices(35) = "Drammen"
+        choices(36) = "Britain"
+        choices(37) = "Pluto"
+        choices(38) = "Nigeria"
+        choices(39) = "Egypt"
+        choices(40) = "Tesla"
+        choices(41) = "Phineas"
+        choices(42) = "Candy"
+        choices(43) = "Marshall Matthers"
+        choices(44) = "Coca Cola"
+        choices(45) = "Bracket"
+        choices(46) = "Food"
+        choices(47) = "3D wallpaper"
+        choices(48) = "3 months"
+        choices(49) = "Elmo"
+        choices(50) = "Skien"
+        choices(51) = "Portugal"
+        choices(52) = "Jupiter"
+        choices(53) = "France"
+        choices(54) = "Ethiopia"
+        choices(55) = "Honda"
+        choices(56) = "Tom"
+        choices(57) = "Rice"
+        choices(58) = "Nothing"
+        choices(59) = "Royal"
+
+
+        answers(0) = "b"c
+        answers(1) = "a"c
+        answers(2) = "d"c
+        answers(3) = "b"c
+        answers(4) = "b"c
+        answers(5) = "a"c
+        answers(6) = "c"c
+        answers(7) = "c"c
+        answers(8) = "a"c
+        answers(9) = "d"c
+        answers(10) = "b"c
+        answers(11) = "a"c
+        answers(12) = "c"c
+        answers(13) = "a"c
+        answers(14) = "c"c
+
+        ques(0) = "Which symbol is technically called an octothorpe?"
+        ques(1) = "The word 'orange' was first used to describe the _____?"
+        ques(2) = "Bubble Wrap was originally intended to be used as?"
+        ques(3) = "How long can a snail sleep?"
+        ques(4) = "What is the real name of Cookie Monster?"
+        ques(5) = "Which country in norway freezes every winter"
+        ques(6) = "Where did the word 'soccer' come from?"
+        ques(7) = "Which planet’s average distance is 3,670,050,000 miles from the sun?"
+        ques(8) = "Which country has a population of more than 1 billion people?"
+        ques(9) = "Which is the hottest inhabited place on earth?"
+        ques(10) = "which company was originally called Backrub?"
+        ques(11) = "Who was the only non-human to testify before the congress?"
+        ques(12) = "Which is the most shoplifted food item in the U.S.? "
+        ques(13) = "What does m & m stand for?"
+        ques(14) = "What was the first soft drink in space?"
+
+        Cont_Load(Randomizer())
+        'set timer
+        min = 15
+        sec = 0
+
+        'set format for timer
+        Label2.Text = Format(min, "00") & " : " & Format(sec, "00")
+        'contains counter
+        Dim ctr As Integer
+        ctr = 0
+        flag = 0
+        Timer1.Start()
+        startNum = Randomizer()
+
+        'will only do something if one is returned
+        While (ctr < 10 Or (min = 0 And sec = 0))
+            If (flag <> 0) Then
+                Select Case flag
+                    Case 1
+                        ctr = ctr + 1
+                        Cont_Load(Randomizer())
+                    Case 2
+                        Cont_Load(Randomizer())
+                    Case 3
+                        ctr = 0
+                        Cont_Load(Randomizer())
+                End Select
+
+                flag = 0
+            End If
+        End While
+    End Sub
+    'loads question
+    Private Sub Cont_Load(ind As Integer)
+        Label1.Text = "Question: " & ques(ind)
+        a.Text = "A. " & choices(ind)
+        b.Text = "B. " & choices(ind + 15)
+        c.Text = "C. " & choices(ind + 30)
+        d.Text = "D. " & choices(ind + 45)
+
+    End Sub
+
+    'Function for creating random Integers for index display
+    Private Function Randomizer() As Integer
+        Dim value As Integer
+        Return value = CInt(Int(15 * Rnd()))
+    End Function
+
+    'Checks the answers from button click and increment the new 
+    'Will return 1 to progress quiz
+    Private Sub Check(v As Char)
+        If v = answers(startNum) Then
+            score = score + 1
+        End If
+        flag = 1
+    End Sub
+
+    Private Sub Go_Click(sender As Object, e As EventArgs) Handles Go.Click
+        If (Go.Visible = True) Then
+            Timer1.Enabled = True
+            Go.Visible = False
+            Pause.Visible = True
+        End If
+    End Sub
+
+    Private Sub Pause_Click(sender As Object, e As EventArgs) Handles Pause.Click
+        If (Pause.Visible = True) Then
+            Timer1.Enabled = False
+            Pause.Visible = False
+            Go.Visible = True
+        End If
+    End Sub
+
+    Private Sub a_Click(sender As Object, e As EventArgs) Handles a.Click
+        If (Timer1.Enabled = True) Then
+            Check("a"c)
+        End If
+    End Sub
+
+    Private Sub b_Click(sender As Object, e As EventArgs) Handles b.Click
+        If (Timer1.Enabled = True) Then
+            Check("b"c)
+        End If
+    End Sub
+
+    Private Sub c_Click(sender As Object, e As EventArgs) Handles c.Click
+        If (Timer1.Enabled = True) Then
+            Check("c"c)
+        End If
+    End Sub
+
+    Private Sub d_Click(sender As Object, e As EventArgs) Handles d.Click
+        If (Timer1.Enabled = True) Then
+            Check("d"c)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        flag = 3
+    End Sub
+
+    'For Clock
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Main()
+        sec = sec - 1
+        If (sec < 0) Then
+            min = min - 1
+            sec = 59
+        End If
+
+        Label2.Text = Format(min, "00") & " : " & Format(sec, "00")
+    End Sub
+End Class
